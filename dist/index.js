f14f6bad6ea2fedb9811c2fc622a39e7f15e1b5f
Christopher Skene <chris@xtfer.com>
2025-10-24 11:16:39 +1100

    chore: ignore dist directory in version control

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-// JMIX TypeScript Library - Main Entry Point
-// JSON Medical Interchange format for secure medical data exchange
-export { JmixBuilder } from './JmixBuilder.js';
-export { SchemaValidator, } from './validation/SchemaValidator.js';
-export { DicomProcessor } from './dicom/DicomProcessor.js';
-// Export all types
-export * from './types/index.js';
-//# sourceMappingURL=index.js.map
